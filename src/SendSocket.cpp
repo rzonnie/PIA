0059e0330aca830a9dc64baa4a5c0ea42c09b933
Remi Jonkman <rb.jonkman@gmail.com>
2016-04-10 11:45:12 +0200

    SendSocket now uses a superclass

--- src/SendSocket.cpp
+++ src/SendSocket.cpp
@@ -1,36 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   sendSocket.cpp
  * Author: remi
- * 
+ * Group: 9
  * Created on April 7, 2016, 2:33 PM
  */
 
 #include "../include/SendSocket.h"
 #include "../include/Settings.h"
 
-SendSocket::SendSocket(Settings* settings, DynamicQueue* dynamicQueue) {
-    this->settings = settings;
-    this->dynamicQueue = dynamicQueue;
-    try {
-        /**
-         * Create a new datagram socket
-         */
-        if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
-            throw std::runtime_error("Socket creation failed");
+SendSocket::SendSocket(Settings* settings, DynamicQueue* dynamicQueue)
+: Socket(settings, dynamicQueue) {
+    init();
+}
 
+void SendSocket::init() {
+    try {
         /**
          * Enable/Disable loopback. (loop = 0 is disabled, loop =1 is enabled)
          * This allows you to receive multicast packets you yourself have send.
          * Obviously enabling this is not really useful for anything but testing
          */
         char loop = 0;
-        if (setsockopt(sock, IPPROTO_IP, IP_MULTICAST_LOOP, (char *) &loop, sizeof (loop)) < 0)
+        if (setsockopt(sockID, IPPROTO_IP, IP_MULTICAST_LOOP, (char *) &loop, sizeof (loop)) < 0)
             throw std::runtime_error("Failed to disable loopback");
 
         /**
@@ -39,7 +30,7 @@ SendSocket::SendSocket(Settings* settings, DynamicQueue* dynamicQueue) {
          */
         struct in_addr local = {0};
         local.s_addr = settings->getLocalIP();
-        if (setsockopt(sock, IPPROTO_IP, IP_MULTICAST_IF, (char *) &local, sizeof (local)) < 0)
+        if (setsockopt(sockID, IPPROTO_IP, IP_MULTICAST_IF, (char *) &local, sizeof (local)) < 0)
             throw std::runtime_error("Local interface failed");
     } catch (std::exception &e) {
         std::cout << e.what() << std::endl;
@@ -48,15 +39,12 @@ SendSocket::SendSocket(Settings* settings, DynamicQueue* dynamicQueue) {
 }
 
 SendSocket::~SendSocket() {
-    if (sock != -1)
-        close(sock);
-    std::cout << "Thread now closed" << std::endl;
 }
 
 void SendSocket::run() {
     while (true) {
         sleep(1);
-        PIA packet = dynamicQueue->retrievePacket();
+        PIA packet = queue->retrievePacket();
         if (packet.getDestinationAddress() > 0)
             sendPacket(packet);
     }
@@ -67,7 +55,6 @@ void SendSocket::sendPacket(PIA &packet) {
      * Fill in the sockaddr_in to specify the destination of datagrams
      * In this case this is simply a port and multicast group address
      */
-
     struct sockaddr_in multicastSender = {0};
     multicastSender.sin_family = AF_INET;
     multicastSender.sin_addr.s_addr = settings->getMulticastGroup();
@@ -79,8 +66,8 @@ void SendSocket::sendPacket(PIA &packet) {
     //read a packet
     packet.getData(buffer);
 
-    //send a packet every 5 seconds
-    if (sendto(sock, buffer, packet.size(), 0, (struct sockaddr*) &multicastSender, sizeof (struct sockaddr_in)) < 0) //sent a UDP packet containing our example data
+    // send the actual packet
+    if (sendto(sockID, buffer, packet.size(), 0, (struct sockaddr*) &multicastSender, sizeof (struct sockaddr_in)) < 0) //sent a UDP packet containing our example data
         perror("Sendto failed");
     //printf("Packet of size %d sent!\n", (int) packet.size());
 }
