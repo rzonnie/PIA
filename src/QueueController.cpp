3129d937ecd443569f6a5fa6276760db0e8fbfc0
Remi Jonkman <rb.jonkman@gmail.com>
2016-04-11 09:41:21 +0200

    Merge

--- src/QueueController.cpp
+++ src/QueueController.cpp
@@ -1,49 +1,48 @@
-#include "../QueueController.h"
+#include "../include/QueueController.h"
 
 QueueController::QueueController(BlockingQueue<std::string> *recqin, int MQZ, int MPL, DynamicQueue DynamicQueueIn);
 {
-	//Hope I don't have to assign the
-	recq = recqin;
-	dynamicQueue = DynamicQueueIn;s
-	MaxQueueSize = MQZ;
-	MaxPacketLength = MPL;
+    //Hope I don't have to assign the
+    recq = recqin;
+    dynamicQueue = DynamicQueueIn;
+    s
+    MaxQueueSize = MQZ;
+    MaxPacketLength = MPL;
 }
 
-QueueController::~QueueController() {};
-	
-BlockingQueue<std::string> * getReceivingQueue() 
-{
-	return recq;
-}
-	
-DynamicQueue QueueController::getDynamicQueue()
-{
-	return dynamicQueue;
-}
-	
-int QueueController::getMaxQueueSize() 
-{
-	return MaxQueueSize;
-}
-	
-int QueueController::getMaxPacketLength() 
-{
-	return MaxPacketLength
-}
-	
-uint32_t QueueController::pollDefaultQueue() 
-{
-	return (dynamicQueue.getFirstDefaultQueueElement);
-}
-	
-void QueueController:: queueSizeChecker()
-{
-	if (dynamicQueue.getDefaultQueuedElements()->defaultQueuedElements.size() > MaxQueueSize)
-	{
-		//Get number of top packet, then destroy that one. Call queueSizeChecker() again.
-	}
+QueueController::~QueueController() {
+};
+
+BlockingQueue<std::string> * getReceivingQueue() {
+    return recq;
 }
 
-void QueueController::ackChecker(uint32_t seqNumber)
-{
-	if (seqNumber == ReceivingPia.readData()
+DynamicQueue QueueController::getDynamicQueue() {
+    return dynamicQueue;
+}
+
+int QueueController::getMaxQueueSize() {
+    return MaxQueueSize;
+}
+
+int QueueController::getMaxPacketLength() {
+    return MaxPacketLength
+}
+
+uint32_t QueueController::pollDefaultQueue() {
+    return (dynamicQueue.getFirstDefaultQueueElement);
+}
+
+void QueueController::queueSizeChecker() {
+    if (dynamicQueue.getDefaultQueuedElements()->defaultQueuedElements.size() > MaxQueueSize) {
+        //Get number of top packet, then destroy that one. Call queueSizeChecker() again.
+    }
+}
+
+void QueueController::ackChecker(uint32_t seqNumber) {
+    
+}
+
+void QueueController::ntaChecker() {
+    
+}
